eadf8e48b7d8ea420190b8f4679401848d974729
IATec -Pedro Henrique da Silva Sebastião <pedro.sebastiao@iatec.com>
2021-05-26 11:59:19 -0300

    Carrossel base finalizado

--- custom.js
+++ custom.js
@@ -1,10 +1,28 @@
 $(function () {
-    $(".botaoLeft").click(function (e) { 
+
+    var numeroItens = $(".carrossel .item").length;
+    var widthItem = parseInt($(".carrossel .item").outerWidth(true));
+    var widthTotal = widthItem * numeroItens;
+    $(".carrossel").css('width', widthTotal)
+
+    var i = 1;
+
+    $(".botaoLeft").click(function (e) {
         e.preventDefault();
-        console.log("click left!");
+        if (i != 1) {
+            i--;
+            $(".carrossel").animate({ 'margin-left': `+=${widthItem}px` }, '500')
+        } else {
+            //console.log(numeroItens, i)
+        }
     });
-    $(".botaoRight").click(function (e) { 
-        e.preventDefault();
-        console.log("click right!");
+    $(".botaoRight").click(function (e) {
+        if (i < numeroItens) {
+            i++;
+            e.preventDefault();
+            $(".carrossel").animate({ 'margin-left': `-=${widthItem}px` }, '500');
+        } else {
+            //console.log(numeroItens, i)
+        }
     });
 });
