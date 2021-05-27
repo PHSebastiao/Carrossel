84aad262dde9f071d22cbb28be862bcda9cd39c0
IATec -Pedro Henrique da Silva Sebastião <pedro.sebastiao@iatec.com>
2021-05-27 17:41:32 -0300

    Rolagem automática

--- custom.js
+++ custom.js
@@ -2,42 +2,70 @@ $(function () {
 
     var numeroItens = $(".carrossel .item").length;
     var widthItem = parseInt($(".carrossel .item").css('width'));
+    var heightItem = parseInt($(".carrossel .item img").css('height'));
     var widthTotal = widthItem * numeroItens;
-    $(".carrossel").css('width', widthTotal)
+    $("#content").width(widthItem).height(heightItem);
+    $(".carrossel").css('width', widthTotal).height(heightItem);
 
     var i = 0;
+
+    var rolagemAutomatica = setInterval(function () { rolagemDireita(); }, 3000);
+
+    $("#content").hover(
+        function () {
+            clearInterval(rolagemAutomatica);
+        },
+        function () {
+            rolagemAutomatica = setInterval(function () { rolagemDireita() }, 3000);
+        }
+    );
+
 
     $(".botaoLeft").click(function (e) {
         e.preventDefault();
         i--;
         if (i == -1) i = numeroItens - 1;
-        let a = parseInt($(".carrossel").css('margin-left'))
-        // console.log(i, numeroItens, a, widthTotal-a);
-        if (widthTotal == widthTotal - a) {
-            $(".carrossel li:last").insertBefore($(".carrossel li:first"));
-            $(".carrossel").css('margin-left', `-=${widthItem}px`);
-            // console.log("oi")
-        }
         rolagemEsquerda();
     });
 
     $(".botaoRight").click(function (e) {
         e.preventDefault();
         i++;
-        let a = -parseInt($(".carrossel").css('margin-left'))
         if (i == numeroItens) i = 0;
-        // console.log(i, numeroItens, a, widthTotal-a);
-        if (widthTotal - a == widthItem) {
-            $(".carrossel li:first").insertAfter($(".carrossel li:last"));
-            $(".carrossel").css('margin-left', `+=${widthItem}px`);
-            // console.log("oie")
-        }
         rolagemDireita();
     });
     function rolagemEsquerda() {
-        $(".carrossel").animate({ 'margin-left': `+=${widthItem}px` }, '500');
+        let a = parseInt($(".carrossel").css('margin-left'));
+        if (widthTotal == widthTotal - a) {
+            $(".carrossel li:last").insertBefore($(".carrossel li:first"));
+            $(".carrossel").css('margin-left', `-=${widthItem}px`);
+        }
+
+        $(".carrossel").animate({ 'margin-left': `+=${widthItem}px` }, {
+            duration: 500,
+            progress: function () {
+                $('.botaoLeft').addClass("disabled")
+            },
+            done: function () {
+                $('.botaoLeft').removeClass("disabled")
+            }
+        });
     }
     function rolagemDireita() {
-        $(".carrossel").animate({ 'margin-left': `-=${widthItem}px` }, '500');
+        let a = -parseInt($(".carrossel").css('margin-left'))
+        if (widthTotal - a == widthItem) {
+            $(".carrossel li:first").insertAfter($(".carrossel li:last"));
+            $(".carrossel").css('margin-left', `+=${widthItem}px`);
+        }
+
+        $(".carrossel").animate({ 'margin-left': `-=${widthItem}px` }, {
+            duration: 500,
+            progress: function () {
+                $('.botaoRight').addClass("disabled");
+            },
+            done: function () {
+                $('.botaoRight').removeClass("disabled");
+            }
+        });
     }
 });
