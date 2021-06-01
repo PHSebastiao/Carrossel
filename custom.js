511e38b822ccf4421a610a1f13c12d4ae76e7801
IATec -Pedro Henrique da Silva Sebastião <pedro.sebastiao@iatec.com>
2021-06-01 08:23:04 -0300

    Botões de navegação funcionais

--- custom.js
+++ custom.js
@@ -1,62 +1,49 @@
-$(function () {
-
+$(document).ready(function () {
     var numeroItens = $(".carrossel .item").length;
-    var widthItem = parseInt($(".carrossel .item").css('width'));
-    var heightItem = parseInt($(".carrossel .item img").css('height'));
+    var widthItem = parseInt($(".carrossel .item").css("width"));
+    var heightItem = parseInt($(".carrossel .item img").css("height"));
     var widthTotal = widthItem * numeroItens;
     $("#content").width(widthItem).height(heightItem);
-    $(".carrossel").css('width', widthTotal).height(heightItem);
+    $(".carrossel").css("width", widthTotal).height(heightItem);
 
     var i = 1;
 
-    // var rolagemAutomatica = setInterval(function () { rolagemDireita(); }, 4000);
+    // Rolagem Automática
+    let timer = 5000;
+    var rolagemAutomatica = setInterval(function () { rolagemDireita(); }, timer);
 
-    // $("#content").hover(
-    //     function () {
-    //         clearInterval(rolagemAutomatica);
-    //     },
-    //     function () {
-    //         rolagemAutomatica = setInterval(function () { rolagemDireita() }, 4000);
-    //     }
-    // );
-
-    for (n = 0; n < numeroItens; n++) {
+    $("#content").hover(
+        function () {
+            clearInterval(rolagemAutomatica);
+        },
+        function () {
+            rolagemAutomatica = setInterval(function () { rolagemDireita() }, timer);
+        }
+    );
+    
+    // Botões de Navegação
+    for (n = 1; n <= numeroItens; n++) {
         $(".botoes-nav").append(`<div class="nav-botao" id="botaonav${n}"></div>`);
     }
+    $("#botaonav1").addClass("active");
 
     $(".nav-botao").click(function (e) {
-        let idNum = 1 + parseInt($(e.currentTarget)[0].id.match(/\d/g));
+        let idNum = parseInt($(e.currentTarget)[0].id.match(/\d/g));
 
         let rolagem = parseInt($(`#img${idNum}`).position().left);
         let posicaoAtual = parseInt($(`#img${i}`).position().left);
+
         i = idNum;
-        // if (rolagem == posicaoAtual) {
-        //     return;
-        // } else if (rolagem == 0) {
-        //     $(".carrossel").animate({ 'margin-left': `0px` });
-        // } else {
-        //     console.log(rolagem, posicaoAtual, rolagem > posicaoAtual)
-        //     $(".carrossel").animate({ 'margin-left': `-=${rolagem}px` });
-        // }
-        // } else if (rolagem > posicaoAtual) {
-        //     rolagem -= posicaoAtual
-        //     $(".carrossel").animate({ 'margin-left': `-=${rolagem}px` });
-        //     i = idNum;
-        // } else if (rolagem < posicaoAtual && rolagem != 0) {
-        //     $(".carrossel").animate({ 'margin-left': `+=${rolagem}px` });
-        //     i = idNum;
-        // } 
+        if (rolagem == posicaoAtual) {
+            return;
+        } else {
+            $(".carrossel").animate({ "margin-left": `-${rolagem}px` });
+            ativarBotao();
+            
+        }
     });
 
-
-
-
-
-
-
-
-
-
+    // Função dos botões
     $(".botaoLeft").click(function (e) {
         e.preventDefault();
 
@@ -69,23 +56,26 @@ $(function () {
         rolagemDireita();
     });
 
-    function rolagemEsquerda() {
+    function rolagemEsquerda(r) {
         i--;
         if (i == 0) i = numeroItens;
-        let a = parseInt($(".carrossel").css('margin-left'));
-        console.log(widthTotal, a, i)
+        let a = parseInt($(".carrossel").css("margin-left"));
+        // console.log(widthTotal, a, i)
         if (widthTotal == widthTotal - a) {
             $(".carrossel li:last").insertBefore($(".carrossel li:first"));
-            $(".carrossel").css('margin-left', `-=${widthItem}px`);
+            $(".carrossel").css("margin-left", "-="+widthItem+"px");
         }
 
-        $(".carrossel").animate({ 'margin-left': `+=${widthItem}px` }, {
+        $(".carrossel").animate({ "margin-left": `+=${widthItem}px` }, {
             duration: 500,
             progress: function () {
-                $('.botaoLeft').addClass("disabled")
+                $(".botaoLeft").addClass("disabled")
+                $(".botoes-nav").addClass("disabled");
+                ativarBotao();
             },
             done: function () {
-                $('.botaoLeft').removeClass("disabled")
+                $(".botaoLeft").removeClass("disabled")
+                $(".botoes-nav").removeClass("disabled");
             }
         });
     }
@@ -93,26 +83,28 @@ $(function () {
     function rolagemDireita() {
         i++;
         if (i == numeroItens + 1) i = 1;
-        let a = -parseInt($(".carrossel").css('margin-left'))
+        let a = -parseInt($(".carrossel").css("margin-left"))
         if (widthTotal - a == widthItem) {
             $(".carrossel li:first").insertAfter($(".carrossel li:last"));
-            $(".carrossel").css('margin-left', `+=${widthItem}px`);
+            $(".carrossel").css("margin-left", `+=${widthItem}px`);
         }
-
-        // console.log("imagem 1: " + parseInt($("#img1").position().left))
-        // console.log("imagem 2: " + parseInt($("#img2").position().left))
-        // console.log("imagem 3: " + parseInt($("#img3").position().left))
-        // console.log("imagem 4: " + parseInt($("#img4").position().left))
-        // console.log("---------------")
-        console.log(widthTotal, a, i)
-        $(".carrossel").animate({ 'margin-left': `-=${widthItem}px` }, {
+        
+        // console.log(widthTotal, a, i)
+        $(".carrossel").animate({ "margin-left": `-=${widthItem}px` }, {
             duration: 500,
             progress: function () {
-                $('.botaoRight').addClass("disabled");
+                $(".botaoRight").addClass("disabled");
+                $(".botoes-nav").addClass("disabled");
+                ativarBotao();
             },
             done: function () {
-                $('.botaoRight').removeClass("disabled");
+                $(".botaoRight").removeClass("disabled");
+                $(".botoes-nav").removeClass("disabled");
             }
         });
+    }
+    function ativarBotao() {
+        $(".active").removeClass("active");
+        $("#botaonav"+i).addClass("active");
     }
 });
